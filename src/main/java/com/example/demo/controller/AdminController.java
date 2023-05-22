eed4b28254da2eae9abedf0086c2ece79c840f23
Przemek <Przemek@HP-Przemek.(none)>
2023-05-22 17:24:26 +0200

    develop: Add controllers with buying and configurating trips

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Trip;
+import com.example.demo.model.TripPurchase;
+import com.example.demo.service.TripPurchaseService;
+import com.example.demo.service.TripService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/trip-configuration")
+public class AdminController {
+
+    private final TripService tripService;
+
+    public AdminController(TripService tripService) {
+        this.tripService = tripService;
+    }
+
+
+    @PostMapping("/add-trip")
+    public ResponseEntity<?> addTripToRepo(@Valid @RequestBody Trip trip) {
+
+    log.info("DODANIE NOWEJ WYCIECZKI: ");
+
+        try {
+            return ResponseEntity.ok(tripService.addTrip(trip));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("INTERNAL SERVER ERROR :: " + exception.getMessage());
+        }
+    }
+
+    @PutMapping("/edit-trip")
+    public ResponseEntity<?> editTripById(@Valid @RequestBody Trip trip) {
+
+        log.info("EDYCJA WYCIECZKI: ");
+
+        try {
+            return ResponseEntity.ok(tripService.editTrip(trip));
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("INTERNAL SERVER ERROR :: " + exception.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/controller/TripPurchaseController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Trip;
+import com.example.demo.service.TripPurchaseService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class TripPurchaseController {
+
+    private final TripPurchaseService tripPurchaseService;
+
+    public TripPurchaseController(TripPurchaseService tripPurchaseService) {
+        this.tripPurchaseService = tripPurchaseService;
+    }
+
+    @PostMapping("/buy-trip/{adults}/{children}")
+    public ResponseEntity<?> buyTrip(
+            @Valid @RequestBody Trip trip,
+            @PathVariable(name = "adults") Short numberOfAdults,
+            @PathVariable(name = "children") Short numberOfChildren
+                    ) {
+
+        try {
+            return ResponseEntity.ok(tripPurchaseService.addTripPurchaseDTO(trip, numberOfAdults, numberOfChildren));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("INTERNAL SERVER ERROR :: " + e.getMessage());
+        }
+    }
+}
